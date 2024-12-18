0418417a0b3d805268155415a7d8aee80fd6af67
Rakkys <rakks.ejk@gmail.com>
2024-12-17 21:06:52 -0700

    Creation of base mod

---
+++ src/main/java/net/rakks/mischieve/MischievousDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rakks.mischieve;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MischievousDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
