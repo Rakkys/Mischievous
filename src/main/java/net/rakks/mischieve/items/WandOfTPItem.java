e307bd32bd15f1abb36704423d9f696c03a3db6a
Rakkys <rakks.ejk@gmail.com>
2024-12-18 08:34:34 -0700

    Removed the users ability to teleport in different dimensions

--- src/main/java/net/rakks/mischieve/items/WandOfTPItem.java
+++ src/main/java/net/rakks/mischieve/items/WandOfTPItem.java
@@ -10,6 +10,7 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 import java.text.MessageFormat;
+import java.util.Objects;
 
 public class WandOfTPItem extends Item {
     public WandOfTPItem(Settings settings) {
@@ -28,12 +29,14 @@ public class WandOfTPItem extends Item {
         double user_X = user.getX();
         double user_Y = user.getY();
         double user_Z = user.getZ();
+        World user_dim = user.getWorld();
 
         nbtData.putDouble("mischieve:WOT_x", user_X);
         nbtData.putDouble("mischieve:WOT_y", user_Y);
         nbtData.putDouble("mischieve:WOT_z", user_Z);
+        nbtData.putString("mischieve:WOT_world", String.valueOf(user_dim));
 
-        user.sendMessage(Text.literal(MessageFormat.format("({0}, {1}, {2})", user_X, user_Y, user_Z)));
+        user.sendMessage(Text.literal(MessageFormat.format("({0}, {1}, {2})", user_X, user_Y, user_Z)), true);
 
         Item.setNbt(nbtData);
     }
@@ -55,6 +58,12 @@ public class WandOfTPItem extends Item {
                 double x = WandOfTP.getNbt().getDouble("mischieve:WOT_x");
                 double y = WandOfTP.getNbt().getDouble("mischieve:WOT_y");
                 double z = WandOfTP.getNbt().getDouble("mischieve:WOT_z");
+                String WOT_world = WandOfTP.getNbt().getString("mischieve:WOT_world");
+
+                if (!Objects.equals(WOT_world, String.valueOf(user.getWorld()))) {
+                    user.sendMessage(Text.translatable("mischieve:WOT.failed_teleport.different_world"), true);
+                    return TypedActionResult.fail(user.getStackInHand(hand));
+                }
 
                 // Work around because I couldn't figure out how to summon this particle normally.
                 user.teleport(user.getX(), user.getY(), user.getZ(), true);
